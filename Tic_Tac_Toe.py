0b135eaa39867935d431365df6052d5ffe54657c
Guedd <37519960+Guedd@users.noreply.github.com>
2019-03-13 14:58:53 +0100

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -9,23 +9,23 @@ C2=0
 C1=0
 cnt=0
 
-def answering(just_play=True,gamer=0):
-    ifgamer==1
-        answer=str(input("Your answer Player 1: Y/N"))
-        if(answer=="Y") or (answer=="y") or (answer=="yes") or (answer=="YES"):
+def answering(just_play=True , gamer=0):
+    if gamer == 1:
+        answer = str(input("Your answer Player 1: Y/N"))
+        if(answer == "Y") or (answer == "y") or (answer == "yes") or (answer == "YES"):
             print(board(game=Tgame,player=1,just_play=play))
         else:
-            just_play=False
-    elif gamer==2:
-        answer=str(input("Your answer Player 2: Y/N"))
-        if(answer=="Y") or (answer=="y") or (answer=="yes") or (answer=="YES"):
+            just_play = False
+    elif gamer == 2:
+        answer = str(input("Your answer Player 2: Y/N"))
+        if(answer == "Y") or (answer == "y") or (answer == "yes") or (answer == "YES"):
             print(board(game=Tgame,player=2,just_play=play))
         else:
-            just_play=False
+            just_play = False
     return just_play
 
 
-def board(game,player=n,just_play=True,line=0,colon=0):
+def board(game , player=0 , just_play=True , line=0 , colon=0):
     global Tgame, L1, L2, C1,C2
     if player==1:
         try:
@@ -33,7 +33,7 @@ def board(game,player=n,just_play=True,line=0,colon=0):
             line=input("witch line: ")
             L1=line
             colon=input("witch colun: ")
-            C1=colon        
+            C1=colon
             if just_play:
                   game[int(L1)][int(C1)]=1
                   print("   0  1  2")
@@ -43,8 +43,8 @@ def board(game,player=n,just_play=True,line=0,colon=0):
             just_play=False
         except IndexError as er:
             print("Error: did you input line\colon  as 0,1 or 2??.",er)
-            answering(just_play=just_play,gamer=player)
-    elif player==2:
+            answering(just_play= just_play , gamer=player)
+    elif player == 2:
         try:
                  print("player 2, chose index now")
                  line=input("witch line: ")
@@ -57,11 +57,11 @@ def board(game,player=n,just_play=True,line=0,colon=0):
                      for count, row in enumerate(game):
                           print(count,row)
                      print("   0  1  2")
-                
+
         except IndexError as er:
             print("Error: did you input line\colon  as 0,1 or 2??.",er)
             answering(just_play=just_play,gamer=player)
-        
+
     if just_play==True:
         return game
     else:
@@ -70,13 +70,14 @@ def board(game,player=n,just_play=True,line=0,colon=0):
 play=True
 while play:
     #player 1
-        print(board(game=Tgame,player=n,just_play=play,line=0,colon=0))
+        print(board(game=Tgame , player=1 , just_play=play , line=0 , colon=0))
 
     #player 2
-        print(board(game=Tgame,player=n,just_play=play,line=0,colon=0))
+        print(board(game=Tgame , player=2 , just_play=play , line=0 , colon=0))
 
     #test of indexing coosing
         if play:
            if (Tgame[int(L1)][int(C1)] == Tgame[int(L2)][int(C2)]):
                 print("the choix of player 2, is  the same of player 1.\n Would you like to make other choix?.")
-                answering(just_play=play)
+                answering(just_play=play , gamer = 2)
+
